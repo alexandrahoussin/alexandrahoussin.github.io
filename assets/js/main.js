4b3e4fddb7bcce210c42d6f0d5ee4accb82fae5f
Alexandra Houssin <alexandra.houss1@gmail.com>
2022-02-11 16:08:08 +0100

    add skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ const navMenu = document.getElementById('nav-menu');
 const navToggle = document.getElementById('nav-toggle');
 const navClose = document.getElementById('nav-close');
     
-console.log(navClose);
+
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
 if(navToggle){
@@ -33,8 +33,24 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills_content');
+const skillsHeader = document.querySelectorAll('.skills__header');
 
+function toggleSkills(){
+    let itemClass = this.parentNode.className
 
+    for(i=0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills_content skills__close'
+    }
+    if(itemClass === 'skills_content skills__close'){
+        this.parentNode.className = 'skills_content skills__open'
+    }
+}
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', toggleSkills)
+})
+
+console.log(skillsContent)
 /*==================== QUALIFICATION TABS ====================*/
 
 
